99f14cb231fc0fffaacec1b446ace9fec963b5ac
imaitomoaki <imaitomoaki@79b840fe-abc6-11de-be8d-735280eae207>
2010-01-22 12:31:50 +0000

    お遊びでブラウザサイズの調整機能を実装しました。

---
+++ BeerGame/webapp/js/main.js
@@ -0,0 +1,7 @@
+function adjustBrowserSize()
+{
+	// window.moveTo(xAxis, yAxis);
+	var width = (screen.width / 2) - 30;
+	var height = (screen.height / 2) - 10;
+	window.resizeTo(width, height);
+};
